3c6cb30f089c28fab8d107c6fe42d0f45ea07d1a
lyyyuna <lyyyuna@outlook.com>
2024-01-17 12:05:06 +0800

    init

---
+++ lib/netbian_crawler.ex
@@ -0,0 +1,18 @@
+defmodule NetbianCrawler do
+  @moduledoc """
+  Documentation for `NetbianCrawler`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NetbianCrawler.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/netbian_crawler_test.exs
@@ -0,0 +1,8 @@
+defmodule NetbianCrawlerTest do
+  use ExUnit.Case
+  doctest NetbianCrawler
+
+  test "greets the world" do
+    assert NetbianCrawler.hello() == :world
+  end
+end
